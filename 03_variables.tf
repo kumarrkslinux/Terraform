3fc6ca50303197cc20ea3218d69024b148e3181b
kumarrkslinux <kumarrkslinux@gmail.com>
2021-12-20 08:26:04 +0530

    Update 03_variables.tf

--- 03_variables.tf
+++ 03_variables.tf
@@ -4,4 +4,18 @@ variable "in_ips" {
     default = "10.10.10.2/32"
 }
 
-variable "username" {}
+variable "username" {
+    type = string
+}
+
+variable "elb_name" {
+    type = string
+}
+
+variable "az_zone" {
+    type = list
+}
+
+variable "timeout" {
+    type = number
+}

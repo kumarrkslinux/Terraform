ffdabe3fd9b5b734688fa39ed2be9345539d730b
kumarrkslinux <kumarrkslinux@gmail.com>
2021-12-20 09:14:23 +0530

    Create map_list_in_variable.tf

---
+++ map_list_in_variable.tf
@@ -0,0 +1,25 @@
+provider "aws"{
+    region = "ap-south-1"
+    access_key = "key"
+    secret_key = "key"
+}
+
+resource "aws_instance" "new_new" {
+    ami = "ami-0851b76e8b1bce90b"
+    #instance_type = var.types[0]
+    instance_type =  var.lists["ap-south-1a"]
+}
+
+variable "types" {
+    type = list
+    default = ["m5.large", "m5.xlarge","t2.medium"]
+}
+
+variable "lists" {
+    type = map
+    default = {
+        ap-south-1a = "t2.micro"
+        ap-south-1b = "t2.nano"
+        ap-south-1c = "t2.small" 
+}
+}

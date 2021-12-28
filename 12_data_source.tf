c4c79c3e8f259909dc224377d954a5c1946eb54b
kumarrkslinux <kumarrkslinux@gmail.com>
2021-12-28 09:54:45 +0530

    Create 12_data_source.tf

---
+++ 12_data_source.tf
@@ -0,0 +1,31 @@
+provider "aws"{
+    region = "eu-central-1"
+    access_key = "key"
+    secret_key = "kay"
+}
+
+data "aws_ami" "ubuntu"{
+    most_recent = true
+    owners = ["099720109477"]
+
+    filter {
+        name = "name"
+        #values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+        values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+            }
+}
+
+/*
+data "aws_vpc" "default_vpc"{
+    default = true
+}
+
+resource "aws_subnet" "new_sub"{
+    vpc_id = "aws_vpc.default_vpc.id"
+    cidr_block = "10.0.0.0/16"
+}
+*/
+resource "aws_instance" "new_vm" {
+    ami = "aws_ami.ubuntu.id"
+    instance_type = "t2.micro"
+}

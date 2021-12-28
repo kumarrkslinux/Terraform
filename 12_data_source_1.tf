a2e7df84bf0ac7178cbcf78eebd3a8e774e4fe10
kumarrkslinux <kumarrkslinux@gmail.com>
2021-12-28 12:22:09 +0530

    Create 12_data_source_1.tf

---
+++ 12_data_source_1.tf
@@ -0,0 +1,22 @@
+provider "aws"{
+    region = "ap-south-1"
+    access_key = "key"
+    secret_key = "key"
+}
+
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners = ["amazon"]
+
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "instance-1" {
+    ami = data.aws_ami.app_ami.id
+   instance_type = "t2.micro"
+}

a7418dafaa88e7b7790acfe534472b127619519b
root <root@centos8.kms.com>
2022-01-23 12:32:17 +0530

    Read, Generate, Modify Configurations

---
+++ git.cmd
@@ -0,0 +1,7 @@
+echo "# file" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:kumarrkslinux/file.git
+git push -u origin main
